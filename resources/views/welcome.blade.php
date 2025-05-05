4eb56e6c3560c2154c42fffef1b372a90420546b
Mhd Khair <mhd.khalifeh2004@gmail.com>
2025-05-05 16:23:07 +0300

    jihu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,20 +9,19 @@
     <meta name="keywords" content="modern lighting, LED lights, chandeliers, home lighting, commercial lighting, energy-efficient lighting, pendant lights, outdoor lighting, smart lighting, decorative lighting, Vallencia Lighting, modern LED chandeliers, luxury lighting, wall sconces, recessed lighting">
     <meta name="robots" content="index, follow">
     <!-- Open Graph Meta Tags (for social media sharing) -->
-    <meta property="og:title" content="Modern Lighting Solutions | Vallencia Lighting">
+    <meta property="og:title" content="Vallencia Lighting">
     <meta property="og:description" content="Explore Vallencia’s range of modern lighting, including LED lights and chandeliers for homes and businesses. Shop now!">
-    <meta property="og:image" content="https://vallencialighting.com/storage/logo/vallencia logo.png">
+    <meta property="og:image" content="https://vallencialighting.com/storage/logo/vallencia-logo.png">
     <meta property="og:url" content="https://vallencialighting.com">
     <meta property="og:type" content="website">
     <!-- Fonts and Favicon -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Alumni+Sans+Pinstripe:ital@0;1&family=Amaranth:ital,wght@0,400;0,700;1,400;1,700&family=Anaheim:wght@400..800&family=Average+Sans&family=Encode+Sans+Condensed:wght@100;200;300;400;500;600;700;800;900&family=Gruppo&family=Jura:wght@300..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Slabo+13px&family=Sulphur+Point:wght@300;400;700&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image" href="/storage/logo/vallencia logo.png" />
+    <link rel="icon" type="image/png" href="/storage/logo/vallencia-logo.png" />
 </head>
 <body>
     <div id="app"></div>
-
     @viteReactRefresh
     @vite(['resources/js/App.jsx', 'resources/css/app.css'])
 </body>
